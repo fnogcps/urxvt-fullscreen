ae0cfabb76232bc7abac8cb75c4c8b59613885eb
Felipe Nogueira <contato.fnog@gmail.com>
2019-11-30 23:25:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgdir="/usr/lib/urxvt/perl/fullscreen"
-su -c "install -Dm644 fullscreen ${pkgdir}"
+sudo install -Dm644 fullscreen ${pkgdir}
 
 if [ -f ${pkgdir} ]; then
     echo "\033[1m\033[32m * \033[00mInstallation finished."
