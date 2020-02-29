5425a4dc1feab5a22f4406159ef323c024ba7659
Felipe Nogueira <contato.fnog@gmail.com>
2020-02-29 06:40:29 -0300

    Fix message format

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ pkgdir="/usr/lib/urxvt/perl/fullscreen"
 sudo install -Dm644 fullscreen ${pkgdir}
 
 if [ -f ${pkgdir} ]; then
-    echo "\e[1m\e[32m [!] \e[00mInstallation finished"
+    echo -e "\e[1m\e[32m[!] \e[00mInstallation finished"
 else
-    echo "\e[1m\e[31m [!] \e[00mInstallation has failed"
+    echo -e "\e[1m\e[31m[!] \e[00mInstallation has failed"
 fi
